58615a681e397a8263ec25a97cd6d8365d75e0f7
Peter Tejeda <johnptejeda@gmail.com>
2020-05-19 16:21:43 -0500

    Swapped repo locations

--- demo/views.py
+++ demo/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
 
 class Another(View):
+
+    books = Book.objects.all()
+    output = ''
+
+    for book in books:
+        output += f"We have {book.title} books in the DB!<br>"
+
     def get(self, request):
-            return HttpResponse('This is another function inside class')
+            return HttpResponse(self.output)
 
 def first(request):
     return HttpResponse('First message from views')
