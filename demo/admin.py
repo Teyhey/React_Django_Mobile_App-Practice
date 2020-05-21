98411e75514cdbf2484f956c5dcac19c847ec358
Peter Tejeda <johnptejeda@gmail.com>
2020-05-20 19:22:28 -0500

    Section 2 : Complete :100:
    On to Section 3, creating Django APIs

--- demo/admin.py
+++ demo/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, BookNumber, Character, Author
+
 
 @admin.register(Book)
-
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'descriptions', 'price']
     list_filter = ['published']
     search_fields = ['title', 'descriptions']
-    
+
+
+admin.site.register(BookNumber)
+admin.site.register(Character)
+admin.site.register(Author)
